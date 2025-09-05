7540ba0eafb9cad3885c36cb4e2d2fe23a68903b
Benoit Armbruster <benoit.armbruster@iqvia.com>
2025-09-05 21:19:50 +0200

    typo fix

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Barm Dev Blog';
-export const SITE_DESCRIPTION = 'Welcome to the Barm Dev Blog!';
+export const SITE_DESCRIPTION = 'A blog about my personal development projects';
 export const SITE_AUTHOR = 'Benoît Armbruster';
